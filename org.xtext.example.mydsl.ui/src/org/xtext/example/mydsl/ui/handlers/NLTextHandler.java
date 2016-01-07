39e575439b17ac533e2332e3c0d4a87ea41ba8f9
André Ribeiro <andre.ribeiro@ist.utl.pt>
2016-01-07 00:59:07 +0000

    Commands & Handlers for all MenuItems :soccer:

---
+++ org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/handlers/NLTextHandler.java
@@ -0,0 +1,15 @@
+package org.xtext.example.mydsl.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class NLTextHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/handlers/WordHandler.java
@@ -0,0 +1,15 @@
+package org.xtext.example.mydsl.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class WordHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
