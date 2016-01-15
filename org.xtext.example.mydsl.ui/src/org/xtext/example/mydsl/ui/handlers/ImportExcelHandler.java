36a7677c4323b54fb7defdedc5f1114456cabb66
André Ribeiro <andre.ribeiro@ist.utl.pt>
2016-01-15 12:47:10 +0000

    FileDialogs added to Import handlers :file_folder:

--- org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/handlers/ImportExcelHandler.java
+++ org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/handlers/ImportExcelHandler.java
@@ -3,12 +3,20 @@ package org.xtext.example.mydsl.ui.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 public class ImportExcelHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		FileDialog dialog = new FileDialog(window.getShell(), SWT.OPEN);
+		dialog.setFilterExtensions(new String[] { "*.xlsx", "*.xls" });
+		dialog.setText("Select the Excel file to upload");
+		dialog.open();
 		return null;
 	}
 
