8a41c6ba4ab7e74d1244c602906703c4922699c8
André Ribeiro <andre.ribeiro@ist.utl.pt>
2016-01-15 15:46:57 +0000

    Menu Type check condition  added

--- org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/handlers/UMLHandler.java
+++ org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/handlers/UMLHandler.java
@@ -4,6 +4,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -11,11 +12,22 @@ public class UMLHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ISelection selection = HandlerUtil.getActiveMenuSelection(event);
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-			window.getShell(),
-			"RSLIL4Privacy",
-			"UML Model generated!");
+		
+		// Check if the command was triggered using the ContextMenu
+		if (selection != null) {
+			MessageDialog.openInformation(
+				window.getShell(),
+				"RSLIL4Privacy",
+				"UML Model generated (ContextMenu)!");
+		} else {
+			MessageDialog.openInformation(
+				window.getShell(),
+				"RSLIL4Privacy",
+				"UML Model generated (MenuBar)!");
+		}
+		
 		return null;
 	}
 
