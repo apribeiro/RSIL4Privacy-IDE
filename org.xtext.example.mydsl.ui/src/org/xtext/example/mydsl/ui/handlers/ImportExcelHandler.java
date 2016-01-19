377e45f645e5345ff9ca76f2ebad8a89327ca398
André Ribeiro <andre.ribeiro@ist.utl.pt>
2016-01-19 22:07:54 +0000

    Statements file partially implemented

--- org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/handlers/ImportExcelHandler.java
+++ org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/handlers/ImportExcelHandler.java
@@ -1,7 +1,6 @@
 package org.xtext.example.mydsl.ui.handlers;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.Iterator;
 
@@ -10,7 +9,6 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.apache.poi.xssf.usermodel.examples.IterateCells;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -36,6 +34,7 @@ public class ImportExcelHandler extends AbstractHandler {
 				Workbook wb = WorkbookFactory.create(inp);
 				sb.append("Package " + dialog.getFileName() + ".Statements.RSLingo4Privacy {");
 				sb.append("\n");
+				sb.append("\n");
 				sb.append("import " + dialog.getFileName() + ".Privatedata.RSLingo4Privacy.*");
 				sb.append("\n");
 				sb.append("import " + dialog.getFileName() + ".Services.RSLingo4Privacy.*");
@@ -57,14 +56,33 @@ public class ImportExcelHandler extends AbstractHandler {
 		    		Cell cellId = row.getCell(0);
 		    		
 		    		if (cellId != null) {
-		    			double id = cellId.getNumericCellValue();
+		    			int id = (int) cellId.getNumericCellValue();
 			    		Cell cellDescription = row.getCell(1);
 			    		String description = cellDescription.getStringCellValue();
-			    		System.out.println("Id: " + id + " Description: " + description);
+			    		Cell cellCondition = row.getCell(2);
+			    		String condition = cellCondition.getStringCellValue();
+			    		Cell cellModality = row.getCell(3);
+			    		String modality = cellModality.getStringCellValue();
+			    		modality = modality.substring(0, 1).toUpperCase() + modality.substring(1);
+			    		Cell cellType = row.getCell(4);
+			    		String type = cellType.getStringCellValue();
+//			    		System.out.println("Id: " + id + " Description: " + description);
+			    		sb.append(type + " st" + id + " {");
+			    		sb.append("\n");
+			    		sb.append("\tDescription \"" + description + "\",");
+			    		sb.append("\n");
+			    		sb.append("\tCondition \"" + condition + "\",");
+			    		sb.append("\n");
+			    		sb.append("\tModality " + modality);
+			    		sb.append("\n};");
+			    		sb.append("\n\n");
 					}
 		    		else
 		    			break;
 				}
+		    	
+		    	sb.deleteCharAt(sb.length() - 1);
+		    	sb.append("};");
 		    	
 				System.out.println(sb);
 //			    FileOutputStream fileOut = new FileOutputStream("C://Users/User/Desktop/Cenas-Result.xlsx");
