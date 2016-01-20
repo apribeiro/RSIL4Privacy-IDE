23ce9655572ad1d16efecb9282f2264d8b1ed89d
André Ribeiro <andre.ribeiro@ist.utl.pt>
2016-01-20 12:16:55 +0000

    Added PrivateData & Services files generation

--- org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/handlers/ImportExcelHandler.java
+++ org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/handlers/ImportExcelHandler.java
@@ -47,75 +47,193 @@ public class ImportExcelHandler extends AbstractHandler {
                     srcGenFolder.create(true, true, new NullProgressMonitor());
 	            }
 				
-				StringBuilder sb = new StringBuilder();
-				InputStream inp = new FileInputStream(filePath);
-				Workbook wb = WorkbookFactory.create(inp);
-				sb.append("Package " + dialog.getFileName() + ".Statements.RSLingo4Privacy {");
-				sb.append("\n");
-				sb.append("\n");
-				sb.append("import " + dialog.getFileName() + ".Privatedata.RSLingo4Privacy.*");
-				sb.append("\n");
-				sb.append("import " + dialog.getFileName() + ".Services.RSLingo4Privacy.*");
-				sb.append("\n");
-				sb.append("import " + dialog.getFileName() + ".Enforcements.RSLingo4Privacy.*");
-				sb.append("\n");
-				sb.append("import " + dialog.getFileName() + ".Recipients.RSLingo4Privacy.*");
-				sb.append("\n");
-				sb.append("\n");
-				
-				// Get the Statements Sheet
-			    Sheet sheet = wb.getSheetAt(0);
-		    	Iterator<Row> rowIt = sheet.rowIterator();
-		    	// Ignore the Header row
-		    	rowIt.next();
-		    	
-		    	while (rowIt.hasNext()) {
-		    		Row row = rowIt.next();
-		    		Cell cellId = row.getCell(0);
-		    		
-		    		if (cellId != null) {
-		    			int id = (int) cellId.getNumericCellValue();
-			    		Cell cellDescription = row.getCell(1);
-			    		String description = cellDescription.getStringCellValue();
-			    		Cell cellCondition = row.getCell(2);
-			    		String condition = cellCondition.getStringCellValue();
-			    		Cell cellModality = row.getCell(3);
-			    		String modality = cellModality.getStringCellValue();
-			    		modality = modality.substring(0, 1).toUpperCase() + modality.substring(1);
-			    		Cell cellType = row.getCell(4);
-			    		String type = cellType.getStringCellValue();
-			    		sb.append(type + " st" + id + " {");
-			    		sb.append("\n");
-			    		sb.append("\tDescription \"" + description + "\",");
-			    		sb.append("\n");
-			    		sb.append("\tCondition \"" + condition + "\",");
-			    		sb.append("\n");
-			    		sb.append("\tModality " + modality);
-			    		sb.append("\n};");
-			    		sb.append("\n\n");
-					}
-		    		else
-		    			break;
-				}
-		    	
-		    	sb.deleteCharAt(sb.length() - 1);
-		    	sb.append("};");
-		    	
-//				System.out.println(sb);
-				
-				IFile fileSt = srcGenFolder.getFile(dialog.getFileName() + "Statements.mydsl");
-				InputStream source = new ByteArrayInputStream(sb.toString().getBytes());
-				
-				if (!fileSt.exists()) {
-					fileSt.create(source, IResource.FORCE, null);
-				} else {
-					fileSt.setContents(source, IResource.FORCE, null);
-				}
+				generateStatementsFile(srcGenFolder, filePath, dialog.getFileName());
+				generatePrivateDataFile(srcGenFolder, filePath, dialog.getFileName());
+				generateServicesFile(srcGenFolder, filePath, dialog.getFileName());
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 		
 		return null;
+	}
+	
+	private void generateStatementsFile(IFolder srcGenFolder, String filePath, String fileName) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		InputStream inp = new FileInputStream(filePath);
+		Workbook wb = WorkbookFactory.create(inp);
+		sb.append("Package " + fileName + ".Statements.RSLingo4Privacy {");
+		sb.append("\n");
+		sb.append("\n");
+		sb.append("import " + fileName + ".PrivateData.RSLingo4Privacy.*");
+		sb.append("\n");
+		sb.append("import " + fileName + ".Services.RSLingo4Privacy.*");
+		sb.append("\n");
+		sb.append("import " + fileName + ".Enforcements.RSLingo4Privacy.*");
+		sb.append("\n");
+		sb.append("import " + fileName + ".Recipients.RSLingo4Privacy.*");
+		sb.append("\n");
+		sb.append("\n");
+		
+		// Get the Statements Sheet
+	    Sheet sheet = wb.getSheetAt(0);
+    	Iterator<Row> rowIt = sheet.rowIterator();
+    	// Ignore the Header row
+    	rowIt.next();
+    	
+    	while (rowIt.hasNext()) {
+    		Row row = rowIt.next();
+    		Cell cellId = row.getCell(0);
+    		
+    		if (cellId != null) {
+    			int id = (int) cellId.getNumericCellValue();
+	    		Cell cellDescription = row.getCell(1);
+	    		String description = cellDescription.getStringCellValue();
+	    		Cell cellCondition = row.getCell(2);
+	    		String condition = cellCondition.getStringCellValue();
+	    		Cell cellModality = row.getCell(3);
+	    		String modality = cellModality.getStringCellValue();
+	    		modality = modality.substring(0, 1).toUpperCase() + modality.substring(1);
+	    		Cell cellType = row.getCell(4);
+	    		String type = cellType.getStringCellValue();
+	    		sb.append(type + " st" + id + " {");
+	    		sb.append("\n");
+	    		sb.append("\tDescription \"" + description + "\",");
+	    		sb.append("\n");
+	    		sb.append("\tCondition \"" + condition + "\",");
+	    		sb.append("\n");
+	    		sb.append("\tModality " + modality);
+	    		sb.append("\n};");
+	    		sb.append("\n\n");
+			}
+    		else
+    			break;
+		}
+    	
+    	sb.deleteCharAt(sb.length() - 1);
+    	sb.append("};");
+    	
+//		System.out.println(sb);
+		
+		IFile fileSt = srcGenFolder.getFile(fileName + "Statements.mydsl");
+		InputStream source = new ByteArrayInputStream(sb.toString().getBytes());
+		
+		if (!fileSt.exists()) {
+			fileSt.create(source, IResource.FORCE, null);
+		} else {
+			fileSt.setContents(source, IResource.FORCE, null);
+		}
+	}
+	
+	private void generatePrivateDataFile(IFolder srcGenFolder, String filePath, String fileName) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		InputStream inp = new FileInputStream(filePath);
+		Workbook wb = WorkbookFactory.create(inp);
+		sb.append("Package " + fileName + ".PrivateData.RSLingo4Privacy {");
+		sb.append("\n");
+		sb.append("\n");
+		
+		// Get the Statements Sheet
+	    Sheet sheet = wb.getSheetAt(3);
+    	Iterator<Row> rowIt = sheet.rowIterator();
+    	// Ignore the Header row
+    	rowIt.next();
+    	
+    	while (rowIt.hasNext()) {
+    		Row row = rowIt.next();
+    		Cell cellId = row.getCell(0);
+    		
+    		if (cellId != null) {
+    			int id = (int) cellId.getNumericCellValue();
+    			Cell cellType = row.getCell(1);
+	    		String type = cellType.getStringCellValue().replaceAll(" ", "");
+    			Cell cellDescription = row.getCell(2);
+	    		String description = cellDescription.getStringCellValue();
+	    		Cell cellAttributes = row.getCell(3);
+	    		String attributes = cellAttributes.getStringCellValue();
+	    		attributes = attributes.substring(0, 1).toUpperCase() + attributes.substring(1);
+	    		
+	    		sb.append("PrivateData PD" + id + " {");
+	    		sb.append("\n");
+	    		sb.append("\tDescription \"" + description + "\",");
+	    		sb.append("\n");
+	    		sb.append("\tType " + type + ",");
+	    		sb.append("\n");
+	    		sb.append("\tAttribute \"" + attributes + "\" Description \"" + attributes + "\"");
+	    		sb.append("\n};");
+	    		sb.append("\n\n");
+			}
+    		else
+    			break;
+		}
+    	
+    	sb.deleteCharAt(sb.length() - 1);
+    	sb.append("};");
+    	
+//		System.out.println(sb);
+		
+		IFile fileSt = srcGenFolder.getFile(fileName + "PrivateData.mydsl");
+		InputStream source = new ByteArrayInputStream(sb.toString().getBytes());
+		
+		if (!fileSt.exists()) {
+			fileSt.create(source, IResource.FORCE, null);
+		} else {
+			fileSt.setContents(source, IResource.FORCE, null);
+		}
+	}
+	
+	private void generateServicesFile(IFolder srcGenFolder, String filePath, String fileName) throws Exception {
+		StringBuilder sb = new StringBuilder();
+		InputStream inp = new FileInputStream(filePath);
+		Workbook wb = WorkbookFactory.create(inp);
+		sb.append("Package " + fileName + ".Services.RSLingo4Privacy {");
+		sb.append("\n");
+		sb.append("\n");
+		sb.append("import " + fileName + ".PrivateData.RSLingo4Privacy.*");
+		sb.append("\n");
+		sb.append("\n");
+		
+		// Get the Statements Sheet
+	    Sheet sheet = wb.getSheetAt(2);
+    	Iterator<Row> rowIt = sheet.rowIterator();
+    	// Ignore the Header row
+    	rowIt.next();
+    	
+    	while (rowIt.hasNext()) {
+    		Row row = rowIt.next();
+    		Cell cellId = row.getCell(0);
+    		
+    		if (cellId != null && cellId.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+    			int id = (int) cellId.getNumericCellValue();
+    			Cell cellName = row.getCell(1);
+	    		String name = cellName.getStringCellValue();
+	    		Cell cellPrivateData = row.getCell(2);
+	    		String privateData = cellPrivateData.getStringCellValue();
+	    		
+	    		sb.append("Service S" + id + " {");
+	    		sb.append("\n");
+	    		sb.append("\tDescription \"" + name + "\",");
+	    		sb.append("\n");
+	    		sb.append("\tRefersTo PrivateData" + privateData + ",");
+	    		sb.append("\n};");
+	    		sb.append("\n\n");
+			}
+    		else
+    			break;
+		}
+    	
+    	sb.deleteCharAt(sb.length() - 1);
+    	sb.append("};");
+    	
+//		System.out.println(sb);
+		
+		IFile fileSt = srcGenFolder.getFile(fileName + "Services.mydsl");
+		InputStream source = new ByteArrayInputStream(sb.toString().getBytes());
+		
+		if (!fileSt.exists()) {
+			fileSt.create(source, IResource.FORCE, null);
+		} else {
+			fileSt.setContents(source, IResource.FORCE, null);
+		}
 	}
 }
