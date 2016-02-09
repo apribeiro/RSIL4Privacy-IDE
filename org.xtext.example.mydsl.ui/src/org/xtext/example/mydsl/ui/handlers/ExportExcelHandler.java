3a1452ad422927bd706d1b3dc9b345ca44954710
André Ribeiro <andre.ribeiro@ist.utl.pt>
2016-02-09 13:12:14 +0000

    Creation of Excel file (WIP) :warning:

--- org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/handlers/ExportExcelHandler.java
+++ org.xtext.example.mydsl.ui/src/org/xtext/example/mydsl/ui/handlers/ExportExcelHandler.java
@@ -1,20 +1,45 @@
 package org.xtext.example.mydsl.ui.handlers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.resource.XtextResourceSet;
+import org.xtext.example.mydsl.MyDslStandaloneSetup;
+import org.xtext.example.mydsl.myDsl.Policy;
 import org.xtext.example.mydsl.ui.windows.MenuCommand;
 import org.xtext.example.mydsl.ui.windows.MenuCommandWindow;
 
+import com.google.inject.Injector;
+
 public class ExportExcelHandler extends AbstractHandler {
 
+	private static final String GEN_FOLDER = "src-gen";
+	private static final String DOCS_FOLDER = "docs";
 	private static final String FILE_EXT = ".mydsl";
+	private static final String DEF_WORD_PATH = "RSL-IL4Privacy-ExcelTemplate.xlsx";
+
+	private final String PLUGIN_PATH = Platform.getInstallLocation()
+			.getURL().getPath().substring(1)
+			+ "plugins/RSLingo4Privacy/";
 	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -42,7 +67,83 @@ public class ExportExcelHandler extends AbstractHandler {
 	}
 
 	private void generateExcel(IFile file) {
-		System.out.println(file.getName() + ".xlsx generated!");
-		// TODO Generate Excel file
+		IProject project = file.getProject();
+		IFolder srcGenFolder = project.getFolder(GEN_FOLDER);
+
+		try {
+			if (!srcGenFolder.exists()) {
+				srcGenFolder.create(true, true, new NullProgressMonitor());
+			}
+
+			IFolder docsFolder = srcGenFolder.getFolder(DOCS_FOLDER);
+
+			if (!docsFolder.exists()) {
+				docsFolder.create(true, true, new NullProgressMonitor());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// Start a new Thread to avoid blocking the UI
+		Runnable runnable = new Runnable() {
+			@Override
+			public void run() {
+				new org.eclipse.emf.mwe.utils.StandaloneSetup().setPlatformUri("../");
+				Injector injector = new MyDslStandaloneSetup().createInjectorAndDoEMFRegistration();
+				XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
+				resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
+				Resource resource = resourceSet.getResource(
+						URI.createURI("platform:/resource/" + file.getFullPath().toString()), true);
+				//URI.createURI("platform:/resource/org.xtext.example.mydsl/src/example.mydsl"), true);
+				Policy policy = (Policy) resource.getContents().get(0);
+
+				try {
+					InputStream from = new FileInputStream(PLUGIN_PATH + DEF_WORD_PATH);
+					XSSFWorkbook  workbook = new XSSFWorkbook(from);
+
+					writeStatements(policy, workbook);
+					writePrivateData(policy, workbook);
+					writeServices(policy, workbook);
+					writeRecipients(policy, workbook);
+					writeEnforcements(policy, workbook);
+
+					// Write the Document in file system
+					File to = new File(project.getLocation().toOSString()
+							+ "/" + GEN_FOLDER + "/" + DOCS_FOLDER 
+							+ "/" + file.getName() + ".xlsx");
+					FileOutputStream out = new FileOutputStream(to);
+					workbook.write(out);
+					out.close();
+					workbook.close();
+
+					project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+
+					System.out.println(file.getName() + ".xlsx generated!");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		new Thread(runnable).start();
+	}
+	
+	private void writeStatements(Policy policy, XSSFWorkbook workbook) {
+		// TODO Auto-generated method stub
+	}
+	
+	private void writeRecipients(Policy policy, XSSFWorkbook workbook) {
+		// TODO Auto-generated method stub
+	}
+	
+	private void writeServices(Policy policy, XSSFWorkbook workbook) {
+		// TODO Auto-generated method stub
+	}
+	
+	private void writePrivateData(Policy policy, XSSFWorkbook workbook) {
+		// TODO Auto-generated method stub
+	}
+	
+	private void writeEnforcements(Policy policy, XSSFWorkbook workbook) {
+		// TODO Auto-generated method stub
 	}
 }
